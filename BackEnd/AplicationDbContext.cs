0a5615ca008c1a7234e64ab3ce9142fe6faf747f
Antonio Andrade <antoniomaresmares001gmail.com>
2021-07-15 21:54:13 -0500

    Primer version

---
+++ BackEnd/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BackEnd.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class AplicationDbContext:DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base (options)
+        {
+
+        }
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; } //TarjetaCredito"nombre de la base de datos"
+
+
+    }
+}
